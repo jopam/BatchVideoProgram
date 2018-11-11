13e68f1c089468ad50deb2d326ed0e74e8d28956
jopam <43388164+jopam@users.noreply.github.com>
2018-11-11 12:29:48 -0600

    Remake
    Remade program using only ffmpy3 as a wrapper for ffmpeg. Much faster copy rather than encoding/decoding every frame. Added txt_file tab

--- short.py
+++ short.py
@@ -5,10 +5,14 @@ Created on Tue Aug 14 10:03:23 2018
 @author: Justin
 """
 
-import cv2
+from tkinter import ttk
 import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 import threading
 import queue
+import ffmpy3
+
 
 exit_flag = False
 
@@ -28,7 +32,6 @@ def shortHelper(txt_file,outq):
         data = file.readlines()
         for line in data:
             inputs.append(line.split(','))
-
     #Iterates through batch process
     for i,case in enumerate(inputs):
         input_file = case[0]
@@ -39,29 +42,12 @@ def shortHelper(txt_file,outq):
         #communicate with updateProgress
         outq.put((i+1,len(inputs)))
         
-        cap = cv2.VideoCapture(input_file)
-        fourcc = cv2.VideoWriter_fourcc(*'MP4V') #mp4 compression
         
-        #Set output video parameters
-        frame_width = int(cap.get(3))
-        frame_height = int(cap.get(4))
-        framerate = cap.get(5)
-        cap.set(0,start*1000) #Start at specified time
-        
-        out = cv2.VideoWriter(output_file,fourcc,framerate,(frame_width,frame_height))
-        
-        #Reads each frame and writes desired frames to output
-        while(cap.isOpened()):
-            ret, frame = cap.read()
-            time = cap.get(0)/1000
-            if ret == True and time <= stop and not exit_flag:
-                out.write(frame)
-            else:
-                break
-        #Clean up
-        cap.release()
-        out.release()
-        cv2.destroyAllWindows()
+        ff = ffmpy3.FFmpeg(
+                inputs = {input_file:None},
+                outputs = {output_file:'-ss {} -to {} -c:v copy -c:a copy'.format(start,stop)}
+                )
+        ff.run()
         if exit_flag:
             break
         
@@ -72,22 +58,73 @@ class myApp:
 
         #Make GUI
         self.root = root
-        root.title("MP4 Batch Break")
-        root.geometry('400x100')
-        root.grid_columnconfigure(0,weight = 0)
-        root.grid_columnconfigure(1,weight = 1)
-        root.grid_columnconfigure(2,weight = 1)
-        root.grid_rowconfigure(0,weight = 1)
-        root.grid_rowconfigure(1,weight = 0)
-        self.txt_file_entry = tk.Entry(root)
-        self.txt_file_entry.grid(row = 0,column = 1,columnspan = 2,sticky = 'ew')
-        tk.Label(root,text = 'Input Text File Path: ').grid(row = 0, column = 0,sticky = 'w')
-        tk.Button(self.root, text='Begin', command= self.beginProgram).grid(row = 1,column = 1,sticky = 'new')
+        self.create_widgets()
         
-        self.progress = tk.Label(root,text = 'File: Not Started')
-
+    def create_widgets(self):
+        self.root.title("MP4 Batch Slice")
+        self.root.geometry('600x150')
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_rowconfigure(0, weight=1)
+        main = ttk.Notebook(self.root)
+        main.grid(sticky='news')
+        page1 = ttk.Frame(main)
+        page1.grid_columnconfigure(1,weight=1)
+        
+        self.txt_file_entry = ttk.Entry(page1)
+        self.txt_file_entry.grid(row = 0,column = 1,sticky = 'ew')
+        ttk.Label(page1,text = 'Input Text File Path: ').grid(row = 0, column = 0)
+        ttk.Button(page1, text='Begin', command= self.beginProgram).grid(row = 1,column = 1)
+        ttk.Button(page1, text='Find', command= lambda: self.find_file(self.txt_file_entry)).grid(row = 0,column = 2)
+        self.progress = ttk.Label(page1,text = 'File: Not Started')
         self.progress.grid(row = 1, column = 2)
         
+        
+        page2 = ttk.Frame(main)
+        page2.grid_columnconfigure(1,weight=1)
+        page2.grid_columnconfigure(4,weight=1)
+        
+        self.source_entry = ttk.Entry(page2)
+        self.source_entry.grid(row = 0,column = 1,sticky='ew')
+        ttk.Label(page2,text = 'Source File: ').grid(row = 0, column = 0)
+        ttk.Button(page2, text='Find', command= lambda: self.find_file(self.source_entry)).grid(row = 0,column = 2)
+        self.dest_entry = ttk.Entry(page2)
+        self.dest_entry.grid(row = 1,column = 1,sticky='ew')
+        ttk.Label(page2,text = 'Destination Folder: ').grid(row = 1, column = 0)
+        ttk.Button(page2, text='Find', command= lambda: self.find_dir(self.dest_entry)).grid(row = 1,column = 2)
+        self.dest_file_entry = ttk.Entry(page2)
+        self.dest_file_entry.grid(row = 1,column = 4,sticky='ew')
+        ttk.Label(page2,text = 'Destination File Name: ').grid(row = 1, column = 3)
+        self.start_entry = ttk.Entry(page2)
+        self.start_entry.grid(row = 2,column = 1)
+        ttk.Label(page2,text = 'Start Time(s): ').grid(row = 2, column = 0)
+        self.stop_entry = ttk.Entry(page2)
+        self.stop_entry.grid(row = 2,column = 3)
+        ttk.Label(page2,text = 'Stop Time(s): ').grid(row = 2, column = 2)
+        self.txt_entry = ttk.Entry(page2)
+        self.txt_entry.grid(row = 3,column = 1,sticky='ew')
+        ttk.Label(page2,text = 'Txt File: ').grid(row = 3, column = 0)
+        ttk.Button(page2, text='Find', command= lambda: self.find_file(self.txt_entry)).grid(row = 3,column = 2)
+        
+        ttk.Button(page2, text='Add to Txt File', command= lambda: self.append_txt()).grid(row = 3,column = 3)
+        
+        main.add(page1, text='Slicing Tab')
+        main.add(page2, text='Text File Adder')
+    def find_file(self,entry):
+        entry.delete(0,tk.END)
+        entry.insert(0,filedialog.askopenfilename())
+    def find_dir(self,entry):
+        entry.delete(0,tk.END)
+        entry.insert(0,filedialog.askdirectory())
+    def append_txt(self):
+        try:
+            field = ','.join([self.source_entry.get(),self.start_entry.get(),
+                              self.stop_entry.get(),self.dest_entry.get()+'/'+self.dest_file_entry.get()])
+        
+            with open(self.txt_entry.get(),'a') as file:
+                file.write('\n' + field)
+        except:
+            messagebox.showerror(title = 'ERROR',message='Oops, something went wrong')
+            
     def beginProgram(self):
         
         outq = queue.Queue()
@@ -114,8 +151,9 @@ class myApp:
 if __name__=='__main__':
     #Interface
     root = tk.Tk()
+    s = ttk.Style()
+    s.theme_use('clam')
     app = myApp(root)
-    
     #Run code
     root.mainloop()
     #Quit Process
